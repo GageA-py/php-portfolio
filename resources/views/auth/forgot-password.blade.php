39e3ba25c6c3a1a952d4da12fd263a1109f70d47
markus <gage_a6969@proton.me>
2024-07-06 19:12:23 -0500

    fix

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,3 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @vite('resources/css/app.css') 
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-ix0mzfeY.css') }}">
+    <script src="{{ secure_asset('build/assets/app-Bg1aHGgo.js') }}" defer></script>
+</head>
+<body>
+    <header class="w-screen border-b-rounded grid grid-cols-2 items-center py-10 lg:grid-cols-3 bg-gray-800">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight pl-10">Gage's Portfolio</span>
+        </div>
+        <div class="flex lg:justify-center lg:col-start-2"></div>
+        @if (Route::has('login'))
+            <nav class="-mx-3 flex flex-1 justify-end pr-10">
+                @auth
+                    <!-- Authenticated links -->
+                @else
+                    <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                        Log in
+                    </a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            Register
+                        </a>
+                    @endif
+                @endauth
+            </nav>
+        @endif
+    </header>
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,9 @@
 <head>
     @vite('resources/css/app.css') 
     <link rel="stylesheet" href="{{ secure_asset('build/assets/app-ix0mzfeY.css') }}">
+    <script src="{{ secure_asset('build/assets/app-Bg1aHGgo.js') }}" defer></script>
 </head>
 <body>
- 
     <header class="w-screen border-b-rounded grid grid-cols-2 items-center py-10 lg:grid-cols-3 bg-gray-800">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
             <span class="font-semibold text-xl tracking-tight pl-10">Gage's Portfolio</span>
@@ -14,7 +14,7 @@
         @if (Route::has('login'))
             <nav class="-mx-3 flex flex-1 justify-end pr-10">
                 @auth
-                
+                    <!-- Authenticated links -->
                 @else
                     <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                         Log in
@@ -29,10 +29,9 @@
         @endif
     </header>
 
-  
     <x-guest-layout>
         <strong>Register to get access to features such as sending emails and subscribing to my newsletter.</strong>
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST" action="{{ secure_url(route('register', [], false)) }}">
             @csrf
 
             <div>
@@ -47,7 +46,6 @@
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
-        
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
                 <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
