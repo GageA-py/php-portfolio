15078bedc904bb91065e946eb4697c9547516443
markus <gage_a6969@proton.me>
2024-07-07 16:36:42 -0500

    fixes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,10 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
-{
+class project extends Model
+{   
+    protected $table = 'project';
     use HasFactory;
-
-    protected $table = 'projects'; // Ensure this matches your table name
 }
-
