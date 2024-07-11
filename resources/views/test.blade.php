4019a1e4e31a832fbac7b3a5a22451cf186d08a7
markus <gage_a6969@proton.me>
2024-07-11 16:18:18 -0500

    fix

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -56,17 +56,18 @@
                     <div class="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden z-10 hidden" id="dropdown">
                         <div class="px-4 py-2">
                             <a href="{{ route('subscribe.page') }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900">Subscribe</a>
-                            <a href="mailto:{{ Auth::user()->email }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900">Contact Me</a>
                             @if (Auth::user()->subscribed)
                                 <div class="border-t border-gray-200"></div>
                                 <a href="{{ route('unsubscribe.page') }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900">Unsubscribe</a>
+                                <div class="border-t border-gray-200"></div>
                             @endif
-                            <div class="border-t border-gray-200"></div>
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit" class="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900 focus:outline-none">Logout</button>
-                            </form>
+                            <a href="mailto:{{ Auth::user()->email }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900">Contact Me</a>
                         </div>
+                        <div class="border-t border-gray-200"></div>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900 focus:outline-none">Logout</button>
+                        </form>
                     </div>
                 </div>
             @else
